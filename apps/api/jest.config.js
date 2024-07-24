8fbdea062805b61bf2fdc80fb5eab211afc70a66
Lucas Candido <lucascandido.ti@gmail.com>
2024-07-24 16:54:50 -0300

    improvement: adjust name mappers on jest.config.ts

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -77,7 +77,9 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '@schedule/api/(.*)$': '<rootDir>/src/$1',
+    '^@schedule/ddd/(.*)$': '<rootDir>/../../packages/ddd/src/$1',
+    '^@schedule/core/(.*)$': '<rootDir>/../../packages/core/src/$1',
+    '^@schedule/api/(.*)$': '<rootDir>/src/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
