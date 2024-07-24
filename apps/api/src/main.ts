06c5a9ca62c1cf16a901b60dc519765d0ba55b37
Lucas Candido <lucascandido.ti@gmail.com>
2024-07-24 10:24:27 -0300

    improvement: initializes the core and ddd libraries to the project and adds them as dependencies for the API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+import { Foo } from '@schedule/core';
+import { AggregateRoot } from '@schedule/ddd';
+
 import { Config } from './config';
 import { AppModule } from './app.module';
 
@@ -12,7 +15,19 @@ async function bootstrap() {
   const config = (configService as unknown as { internalConfig: Config })
     .internalConfig;
 
+  testLocalDependecies();
+
   app.setGlobalPrefix(config.api.prefix);
   await app.listen(+config.api.port);
 }
+
 bootstrap();
+
+function testLocalDependecies() {
+  const core = new Foo();
+  console.log('@schedule/core: test > ', core);
+
+  const ddd = class DDD extends AggregateRoot<any> {};
+
+  console.log('@schedule/ddd: test > ', ddd);
+}
